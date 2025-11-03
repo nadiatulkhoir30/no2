afaf3eb8a52a897b1d503b7a0a23aa3c96244720
Nadiatul Khoir <165635618+nadiatulkhoir30@users.noreply.github.com>
2025-11-03 00:08:12 +0000

    baru

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 # ---------------------------
 # Load model & scaler
